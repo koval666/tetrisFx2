e55b697404df4e1bd0cd92d3b1e675ac7c3aa79a
dkovalenko <d.kovalenko@voskhod.ru>
2020-12-23 23:25:52 +0400

    SimpleUI implementation

--- src/sample/game/Brick.java
+++ src/sample/game/Brick.java
@@ -1,4 +1,8 @@
 package sample.game;
 
+import javafx.scene.paint.Color;
+
 public interface Brick {
+
+    Color getColor();
 }
--- src/sample/ui/javafx/simpleui/SimpleJavaFxUI.java
+++ src/sample/ui/javafx/simpleui/SimpleJavaFxUI.java
@@ -1,6 +1,10 @@
 package sample.ui.javafx.simpleui;
 
+import com.sun.istack.internal.NotNull;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
@@ -12,8 +16,12 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import sample.controller.Controller;
+import sample.game.Brick;
 import sample.game.BrickTable;
 import sample.ui.javafx.JavaFxUI;
+
+import java.awt.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static sample.Configuration.*;
 
@@ -39,6 +47,10 @@ public class SimpleJavaFxUI implements JavaFxUI {
     protected final Label scoreLabel;
     protected final Label linesLabel;
     protected final Label levelLabel;
+
+    ReentrantLock cupUpdateLock = new ReentrantLock();
+    ReentrantLock hintUpdateLock = new ReentrantLock();
+
 
     public SimpleJavaFxUI() {
         cupGrid = new GridPane();
@@ -107,31 +119,86 @@ public class SimpleJavaFxUI implements JavaFxUI {
 
     @Override
     public void updateCup(BrickTable brickTable) {
-
+        if (cupUpdateLock.tryLock()) {
+            try {
+                repaintBrickGrid(cupGrid, brickTable);
+            } finally {
+                cupUpdateLock.unlock();
+            }
+        }
     }
 
     @Override
     public void updateHint(BrickTable brickTable) {
+        if (hintUpdateLock.tryLock()) {
+            try {
+                repaintBrickGrid(hintGrid, brickTable);
+            } finally {
+                hintUpdateLock.unlock();
+            }
+        }
 
+    }
+
+    /**
+     * Point 0:0 is in upper left corner
+     */
+    private void repaintBrickGrid(GridPane grid, BrickTable brickTable) {
+        for (int y = 0; y < brickTable.getVisibleHeight(); y++) {
+            for (int x = 0; x < brickTable.getVisibleWidth(); x++) {
+                Point currentPoint = new Point(x, y);
+
+                if (brickTable.hasBrick(currentPoint)) {
+                    Brick brick = brickTable.getBrick(currentPoint);
+                    getRectangle(grid, currentPoint).setFill(brick.getColor());
+                } else {
+                    getRectangle(grid, currentPoint).setFill(DEFAULT_COLOR);
+                }
+            }
+        }
+    }
+
+    private Rectangle getRectangle(@NotNull GridPane rectangleGrid, Point point) {
+        Node result = null;
+        ObservableList<Node> childrens = rectangleGrid.getChildren();
+
+        for (Node node : childrens) {
+            if (GridPane.getRowIndex(node) != null
+                    && GridPane.getRowIndex(node) == point.getY()
+                    && GridPane.getColumnIndex(node) != null
+                    && GridPane.getColumnIndex(node) == point.getX()) {
+                result = node;
+                break;
+            }
+        }
+
+        return (Rectangle) result;
     }
 
     @Override
     public void setScore(long score) {
-
+        Platform.runLater(() -> {
+            scoreLabel.setText(String.format("%06d", score));
+        });
     }
 
     @Override
     public void setLines(long lines) {
+        Platform.runLater(() -> {
+            linesLabel.setText(String.format("%03d", lines));
+        });
 
     }
 
     @Override
     public void setLevel(long level) {
-
+        Platform.runLater(() -> {
+            levelLabel.setText(String.format("%02d", level));
+        });
     }
 
     @Override
     public void gameOver() {
-
+        System.out.println("Game over!");
     }
 }
